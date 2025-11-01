57d515f67b23df389573bb379cbda635efb109e4
gerrit <gerrit.wanderer@gmail.com>
2025-11-01 16:41:12 +0100

    Add ctrl+n and ctrl+p for history scrolling in snacks

--- .config/nvim/lua/plugins/extend-snacks.lua
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -4,5 +4,15 @@ return {
 		scroll = {
 			enabled = false, -- Disable scrolling animations
 		},
+		picker = {
+			win = {
+				input = {
+					keys = {
+						["<c-n>"] = { "history_forward", mode = { "i", "n" } },
+						["<c-p>"] = { "history_back", mode = { "i", "n" } },
+					},
+				},
+			},
+		},
 	},
 }
